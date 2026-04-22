c5df1a9fcd6f8c4dc9f25e1a0a78806fdf4fc86e
Zahary Karadjov <zahary@metacraft-labs.com>
2026-04-22 18:33:41 +0300

    fix: update for Nim trace writer (close + .ct format tests)

--- src/tracer.rs
+++ src/tracer.rs
@@ -144,6 +144,7 @@ impl PolkaVmTracer {
         TraceWriter::finish_writing_trace_metadata(&mut *tracer.writer)
             .map_err(|e| eyre!("{e}"))?;
         TraceWriter::finish_writing_trace_paths(&mut *tracer.writer).map_err(|e| eyre!("{e}"))?;
+        tracer.writer.close().map_err(|e| eyre!("{e}"))?;
 
         Ok(())
     }
